6ff79518410c1445b2bedefc0e6cca550b7ea5af
wintonBy <winton_by@126.com>
2016-11-22 23:30:10 +0800

    增加scale动画

--- app/src/main/java/com/winton/demo/AnimDemoActivity.java
+++ app/src/main/java/com/winton/demo/AnimDemoActivity.java
@@ -2,15 +2,50 @@ package com.winton.demo;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by winton on 2016/11/18.
  */
 
-public class AnimDemoActivity extends AppCompatActivity {
+public class AnimDemoActivity extends BaseActivity implements View.OnClickListener{
+    private ImageView mIVAnim;
+    private Button mBtScale;
+
+    private Animation scaleAnimation;
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim_demo);
+        mIVAnim = (ImageView)findViewById(R.id.iv_anim);
+        mBtScale = (Button) findViewById(R.id.bt_anim_scale);
+        initData();
+        initListener();
+    }
+
+    private void initData(){
+        scaleAnimation = AnimationUtils.loadAnimation(AnimDemoActivity.this,R.anim.scale);
+    }
+    private void initListener(){
+        mBtScale.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mBtScale){
+            mIVAnim.startAnimation(scaleAnimation);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
